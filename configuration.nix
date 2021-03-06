ee6f8b1ccb344ea593feff043921060427cb891a
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-03-06 13:18:57 -0300

    Enable Plymouth
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "spinner";
 
   boot.supportedFilesystems = ["exfat" "ntfs"];
 
